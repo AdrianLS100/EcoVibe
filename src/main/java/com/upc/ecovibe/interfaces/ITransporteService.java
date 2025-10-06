3d340d70e979a4c6baddb9d9aba97978627f3274
AdrianLS100 <adrianlazo310@gmail.com>
2025-10-05 22:41:03 -0500

    Update ITransporteService.java

--- src/main/java/com/upc/ecovibe/interfaces/ITransporteService.java
+++ src/main/java/com/upc/ecovibe/interfaces/ITransporteService.java
@@ -2,17 +2,10 @@ package com.upc.ecovibe.interfaces;
 
 import com.upc.ecovibe.dtos.TransporteDTO;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface ITransporteService {
     TransporteDTO crear(TransporteDTO dto);
-
     List<TransporteDTO> listarPorActividad(Long actividadId);
-
-    BigDecimal sumarDistanciaKm(Long actividadId);
-
     void eliminar(Long transporteId);
-
-    long eliminarPorActividad(Long actividadId);
 }
