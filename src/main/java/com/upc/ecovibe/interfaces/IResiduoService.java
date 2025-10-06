4b445bb0795e468942e2b702dc6d0678c106ec5d
AdrianLS100 <adrianlazo310@gmail.com>
2025-10-05 22:38:11 -0500

    Update IResiduoService.java

--- src/main/java/com/upc/ecovibe/interfaces/IResiduoService.java
+++ src/main/java/com/upc/ecovibe/interfaces/IResiduoService.java
@@ -2,17 +2,10 @@ package com.upc.ecovibe.interfaces;
 
 import com.upc.ecovibe.dtos.ResiduoDTO;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface IResiduoService {
     ResiduoDTO crear(ResiduoDTO dto);
-
     List<ResiduoDTO> listarPorActividad(Long actividadId);
-
-    BigDecimal sumarPesoKg(Long actividadId);
-
     void eliminar(Long residuoId);
-
-    long eliminarPorActividad(Long actividadId);
 }
